63d88d2a9231cd9f62eac37ad971679ee1e881d0
user88 <ukupnique@gmail.com>
2024-11-11 17:54:27 +0300

    make parse

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,10 @@
 import argparse
+import json
+
+
+def load_json(filepath):
+    with open(filepath, 'r') as file:
+        return json.load(file)
 
 
 def main():
@@ -6,12 +12,16 @@ def main():
 
     parser.add_argument('first_file', help='The path to the first file to compare.')
     parser.add_argument('second_file', help='The path to the second file to compare.')
-
     parser.add_argument('-f', '--format', default='plain', help='set format of output (default: plain)')
 
     args = parser.parse_args()
 
+    data1 = load_json(args.first_file)
+    data2 = load_json(args.second_file)
+
     print(f"Comparing '{args.first_file}' and '{args.second_file}' with format '{args.format}'.")
+    print("Data from first file:", data1)
+    print("Data from second file:", data2)
 
 
 if __name__ == "__main__":
